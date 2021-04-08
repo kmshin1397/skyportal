0f18e39749086cf10bbafcf3c5eb25d21e04fcd9
kshin <kshin@caltech.edu>
2021-04-08 12:42:00 -0400

    fix

--- test_candidate_state.py
+++ test_candidate_state.py
@@ -1,6 +1,7 @@
 from baselayer.app.env import load_env
+from baselayer.app.models import init_db
 from skyportal.models import DBSession, Candidate
 
 env, cfg = load_env()
-
+init_db(**cfg["database"])
 print(DBSession().query(Candidate.id, Candidate.passed_at).all())
