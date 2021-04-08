7e6536ead8f2eab42cef2d2ba2f2f23dcd7190b2
kshin <kshin@caltech.edu>
2021-04-08 12:57:08 -0400

    Use obj id

--- test_candidate_state.py
+++ test_candidate_state.py
@@ -3,5 +3,6 @@ from baselayer.app.models import init_db
 from skyportal.models import DBSession, Candidate
 
 env, cfg = load_env()
+cfg["database"]["database"] = "skyportal_test"
 init_db(**cfg["database"])
 print(DBSession().query(Candidate.obj_id, Candidate.passed_at).all())
